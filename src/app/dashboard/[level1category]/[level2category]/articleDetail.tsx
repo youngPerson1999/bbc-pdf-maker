5eb22e6a17d94cfc664ef5118140e17e22340d9d
young_person <49819098+youngPerson1999@users.noreply.github.com>
2025-06-04 22:06:14 +0900

    [chore] edit close btn deign

--- src/app/dashboard/[level1category]/[level2category]/articleDetail.tsx
+++ src/app/dashboard/[level1category]/[level2category]/articleDetail.tsx
@@ -16,16 +16,18 @@ const ArticleDetail = ({ href, close }: ArticleDetailProps) => {
       animate={{ height: "auto", opacity: 1 }}
       exit={{ height: 0, opacity: 0 }}
       transition={{ duration: 0.3 }}
+      className="p-4"
     >
-      {isLoading && <p className="p-4">Loading...</p>}
+      {isLoading && <p>Loading...</p>}
       {articleContent && (
-        <div className="flex flex-col p-4 w-full">
+        <div className="flex flex-col w-full">
           <h1 className="text-2xl font-bold">{articleContent.headline}</h1>
           <p className="text-right">{articleContent.time}</p>
           <ArticleTextContent content={articleContent.content} />
         </div>
       )}
       <button
+        className="cursor-pointer mt-4 px-4 py-2 bg-gray-200/20 rounded hover:bg-gray-300/20 transition-colors"
         onClick={(e) => {
           e.stopPropagation();
           close();
